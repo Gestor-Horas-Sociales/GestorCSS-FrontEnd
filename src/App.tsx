07675fcd1aa97077194890014a432b8f1729aff8
Gerarvvdor <geraraov@gmail.com>
2025-06-12 14:59:27 -0600

    Updateing home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import React from "react";
-import LoginPage from "./Pages/LogIn/page";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//Pages
+import HomePage from "./Pages/Super_User/HomePage/Home_page";
+import LoginPage from "./Pages/Super_User/LogIn/LogIn_page";
+
 function App() {
   return (
-    <>
-      <LoginPage/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage/>} />
+        <Route path="/home" element={<HomePage/>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/ui/nav-main.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { type LucideIcon } from "lucide-react";
+
+import { Collapsible } from "@/components/ui/collapsible";
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSubItem,
+} from "@/components/ui/sidebar";
+
+export function NavMain({
+  items,
+}: {
+  items: {
+    title: string;
+    url: string;
+    icon: LucideIcon;
+    isActive?: boolean;
+    items?: {
+      title: string;
+      url: string;
+    }[];
+  }[];
+}) {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>Platform</SidebarGroupLabel>
+      <SidebarMenu>
+        {items.map((item) => (
+          <Collapsible key={item.title} asChild defaultOpen={item.isActive}>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild tooltip={item.title}>
+                <a href={item.url}>
+                  <item.icon />
+                  <span>{item.title}</span>
+                </a>
+              </SidebarMenuButton>
+              {item.items?.length ? (
+                <>
+                  {item.items?.map((subItem) => (
+                    <SidebarMenuSubItem key={subItem.title}>
+                    </SidebarMenuSubItem>
+                  ))}
+                </>
+              ) : null}
+            </SidebarMenuItem>
+          </Collapsible>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  );
+}
